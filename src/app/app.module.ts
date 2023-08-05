140fb93cb3bafc554bf206a3362d8dfea297a061
Andre-coder365 <andreoliveira365@hotmail.com>
2023-08-04 21:58:57 -0300

    Validação de formulário CREATE-TECNICO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+//import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -76,6 +77,7 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
       closeButton: true,
       progressBar: true
     })
+    //NgxMaskModule.forRoot()
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
