075e0ffcc7d65279606a39c6492b8fa9ebdf68ed
Andre-coder365 <andreoliveira365@hotmail.com>
2023-08-06 13:51:41 -0300

    Serviço para atualizar CHAMADo implementado

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -11,6 +11,10 @@ export class ChamadoService {
 
   constructor(private http: HttpClient) { }
 
+  findById(id: any): Observable<Chamado> {
+    return this.http.get<Chamado>(`${API_CONFIG.baseUrl}/chamados/${id}`)
+  }
+
   findAll(): Observable<Chamado[]> {
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
   }
@@ -18,6 +22,12 @@ export class ChamadoService {
   create(chamado: Chamado): Observable<Chamado> {
     return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado)
   }
+
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado)
+  }
+
+
 
 
 }
