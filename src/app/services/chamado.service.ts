2b4ebfe36fef9093a34401723f9812c21b836928
Andre-coder365 <andreoliveira365@hotmail.com>
2023-08-06 10:26:35 -0300

    Filtro e método findAll para Chamado

---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Chamado } from '../models/chamado';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
+  }
+
+
+}
